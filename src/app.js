706a4680381368768e30b43d62d1d4d3f8e03ad7
Steven Bull <stevenwbulljr@gmail.com>
2019-10-08 07:29:22 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const errorHandler = require('./errorHandler');
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'tiny'
+  : 'common';
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.get('/', ( req, res ) => {
+  res.send('Hello world!');
+});
+
+app.use(errorHandler);
+
+module.exports = app;
