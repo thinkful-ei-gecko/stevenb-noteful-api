805e816c66fe77e63b62b4725006fadb25c5eb1f
Steven Bull <stevenwbulljr@gmail.com>
2019-10-08 10:30:53 -0500

    add folders and notes service and router files, add logic to folders-service.js

---
+++ folders/folders-service.js
@@ -0,0 +1,37 @@
+const FoldersService = {
+  getAllFolders(knex) {
+    return knex.select('*').from('folders');
+  },
+
+  insertFolder(knex, newFolder) {
+    return knex 
+      .insert(newFolder)
+      .into('folders')
+      .returning('*')
+      .then( rows => rows[0] );
+  },
+
+  getById(knex, id) {
+    return knex 
+      .from('folders')
+      .select('*')
+      .where('id', id)
+      .first();
+  },
+
+  deleteFolder(knex, id) {
+    return knex
+      .from('folders')
+      .where({ id })
+      .delete();
+  },
+
+  updateFolder(knex, id, newFolderFields) {
+    return knex
+      .from('folders')
+      .where({ id })
+      .update(newFolderFields);
+  }
+};
+
+module.exports = FoldersService;
